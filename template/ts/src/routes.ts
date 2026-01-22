26315c5659f4f527d20969ed92139f2b8a40d67d
sheraz4196 <sheraztoor321@gmail.com>
2026-01-22 23:01:12 +0500

    ts: converted into ts

--- template/ts/src/routes.ts
+++ template/ts/src/routes.ts
@@ -1,17 +1,16 @@
 import { Router } from "express";
+
 import {
   getHealth,
   createItem,
   createItemSchema,
-} from "./modules/health/controller.js";
-import { validateRequest } from "./middlewares/validateRequest.js";
+} from "./modules/health/controller";
+import { validateRequest } from "./middlewares/validateRequest";
 
 const router = Router();
 
-// Health check
 router.get("/health", getHealth);
 
-// Example: Create item with validation
 router.post("/items", validateRequest(createItemSchema), createItem);
 
 export default router;
