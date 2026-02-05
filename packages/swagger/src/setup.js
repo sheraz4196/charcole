80c8bd88fd791b33b9797e6eb212b7de7ef29a81
sheraz4196 <sheraztoor321@gmail.com>
2026-02-05 13:54:40 +0500

    fix: slight bug

--- packages/swagger/src/setup.js
+++ packages/swagger/src/setup.js
@@ -35,39 +35,3 @@ function setupSwagger(app, options = {}) {
 
   return openApiSpec;
 }
-
-import swaggerUi from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jsdoc";
-
-export function setupSwagger(app, options = {}) {
-  const defaultOptions = {
-    title: "Charcole API",
-    version: "1.0.0",
-    description: "Auto-generated API documentation",
-    path: "/api-docs",
-    servers: [{ url: "http://localhost:3000", description: "Local server" }],
-  };
-
-  const config = { ...defaultOptions, ...options };
-
-  const openApiSpec = swaggerJSDoc({
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: config.title,
-        version: config.version,
-        description: config.description,
-      },
-      servers: config.servers,
-    },
-    apis: ["./src/modules/**/*.js", "./src/routes/**/*.js"],
-  });
-
-  app.use(config.path, swaggerUi.serve, swaggerUi.setup(openApiSpec));
-
-  console.log(
-    `705 Swagger UI available at http://localhost:${process.env.PORT || 3000}${config.path}`,
-  );
-
-  return openApiSpec;
-}
